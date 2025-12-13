b83491f7944c04d2d8210c72015a50c663638c1c
HSYStudio <hsystudio.dev@gmail.com>
2025-12-14 01:41:54 +0900

    [Feat/#11] 라인업 리스트 조회 API 연동

--- app/src/main/java/com/hsystudio/valtips/data/remote/api/LineupApi.kt
+++ app/src/main/java/com/hsystudio/valtips/data/remote/api/LineupApi.kt
@@ -1,5 +1,6 @@
 package com.hsystudio.valtips.data.remote.api
 
+import com.hsystudio.valtips.data.remote.dto.LineupListResponseDto
 import com.hsystudio.valtips.data.remote.dto.LineupStatusDto
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -16,4 +17,19 @@ interface LineupApi {
     suspend fun getAgentLineupStatus(
         @Query("map_uuid") mapUuid: String
     ): List<LineupStatusDto>
+
+    // 요원 + 맵 기준 전체 라인업 리스트 조회
+    @GET("lineups")
+    suspend fun getLineups(
+        @Query("agent_uuid") agentUuid: String,
+        @Query("map_uuid") mapUuid: String
+    ): LineupListResponseDto
+
+    // 타임스탬프 이후 추가된 라인업 리스트 조회
+    @GET("lineups/updates")
+    suspend fun getLineupUpdates(
+        @Query("agent_uuid") agentUuid: String,
+        @Query("map_uuid") mapUuid: String,
+        @Query("since") since: String
+    ): LineupListResponseDto
 }
