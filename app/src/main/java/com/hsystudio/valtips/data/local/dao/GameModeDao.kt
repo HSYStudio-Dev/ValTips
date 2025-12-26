c6e718d0ef9564a08b23cbe99ce77a3f8da20437
HSYStudio <hsystudio.dev@gmail.com>
2025-12-26 18:18:15 +0900

    [Refactor/#15] Dao 코드 최적화

--- app/src/main/java/com/hsystudio/valtips/data/local/dao/GameModeDao.kt
+++ app/src/main/java/com/hsystudio/valtips/data/local/dao/GameModeDao.kt
@@ -4,19 +4,14 @@ import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
 import com.hsystudio.valtips.data.local.entity.GameModeEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GameModeDao {
+    // 초기 리소스 저장
     @Upsert
     suspend fun upsert(items: List<GameModeEntity>)
 
-    @Query("SELECT * FROM game_modes ORDER BY displayName ASC")
-    suspend fun getAll(): List<GameModeEntity>
-
+    // 초기 리소스 초기화
     @Query("DELETE FROM game_modes")
     suspend fun clearAll()
-
-    @Query("SELECT * FROM game_modes ORDER BY displayName ASC")
-    fun observeAll(): Flow<List<GameModeEntity>>
 }
