5fb8d3592c2e42fcf117a929a35a8a44b76f6433
HSYStudio-Dev <hsystudio.dev@gmail.com>
2026-02-25 16:03:51 +0900

    [refactor/#27] 광고 ID 교체 (#28)

--- app/src/main/java/com/hsystudio/valtips/data/ad/AdConfig.kt
+++ app/src/main/java/com/hsystudio/valtips/data/ad/AdConfig.kt
@@ -8,33 +8,33 @@ object AdConfig {
 
     // 요원 화면용
     val AgentsNativeId: String
-        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-3940256099942544/2247696110"
+        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-9443621239707976/5118108344"
 
     // 요원 상세 화면용
     val AgentDetailNativeId: String
-        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-3940256099942544/2247696110"
+        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-9443621239707976/5366713291"
 
     // 맵 화면용
     val MapsNativeId: String
-        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-3940256099942544/2247696110"
+        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-9443621239707976/7305287982"
 
     // 맵 상세 화면용
     val MapDetailNativeId: String
-        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-3940256099942544/2247696110"
+        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-9443621239707976/5022410000"
 
     // 맵 선택 화면용
     val MapSelectNativeId: String
-        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-3940256099942544/2247696110"
+        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-9443621239707976/2740549958"
 
     // 요원 선택 화면용
     val AgentSelectNativeId: String
-        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-3940256099942544/2247696110"
+        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-9443621239707976/1427468289"
 
     // 라인업 상세 화면용
     val LineupDetailNativeId: String
-        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-3940256099942544/2247696110"
+        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-9443621239707976/9651920152"
 
     // 설정 화면용
     val SettingNativeId: String
-        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-3940256099942544/2247696110"
+        get() = if (BuildConfig.DEBUG) TEST_ID else "ca-app-pub-9443621239707976/3754248721"
 }
